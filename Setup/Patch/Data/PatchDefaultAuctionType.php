db62a34a622e3f40aafae5c10762e723bd0fcaf0
Francisco Huerta Yumha <fjhuerta.y@gmail.com>
2023-07-05 21:03:55 +0000

    first(upload)

---
+++ Setup/Patch/Data/PatchDefaultAuctionType.php
@@ -0,0 +1,44 @@
+<?php
+namespace Bidder\CustomSellerOptions\Setup\Patch\Data;
+
+use Magento\Eav\Setup\EavSetup;
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+
+class PatchDefaultAuctionType implements DataPatchInterface
+{
+    private $moduleDataSetup;
+    private $eavSetupFactory;
+
+    public function __construct(
+        ModuleDataSetupInterface $moduleDataSetup,
+        EavSetupFactory $eavSetupFactory
+    ) {
+        $this->moduleDataSetup = $moduleDataSetup;
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+
+    public function apply()
+    {
+        /** @var EavSetup $eavSetup */
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $this->moduleDataSetup]);
+
+        $eavSetup->updateAttribute(
+            \Magento\Catalog\Model\Product::ENTITY,
+            'auction_type',
+            'default_value',
+            0
+        );
+    }
+
+    public static function getDependencies()
+    {
+        return [];
+    }
+
+    public function getAliases()
+    {
+        return [];
+    }
+}
---
+++ registration.php
@@ -0,0 +1,7 @@
+<?php
+use \Magento\Framework\Component\ComponentRegistrar;
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Bidder_CustomSellerOptions',
+    __DIR__
+);
